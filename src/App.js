22c06b476241a80331dead16814719ad4baf106c
js00070 <909645105@qq.com>
2019-12-05 00:15:20 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 import {Button} from "antd";
 
+import axios from "axios"
+
 import { Layout, Menu, Breadcrumb, Icon, List, Pagination, Row, Col, Table, Divider, Tag, Input } from 'antd';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
 
+import * as Flux from "flux/utils";
+
+import { ReduceStore } from "flux/utils";
+const { CheckableTag } = Tag;
 const { TextArea } = Input;
 
 const { SubMenu } = Menu;
@@ -61,6 +67,8 @@ const problems_columns = [
   },
 ];
 
+// window.menukey = '2'
+
 const commit_columns = [
   {
     title: '提交id',
@@ -68,24 +76,29 @@ const commit_columns = [
     key: 'commit_id',
   },
   {
+    title: '提交时间',
+    dataIndex: 'commit_time',
+    key: 'commit_time'
+  },
+  {
     title: '题目标题',
     dataIndex: 'problem_title',
     key: 'problem_title',
-    render: t => <a href={t.link}>{t.title}</a>,
+    render: t => <a onClick={()=>{window.menukey='3'}} href={t.link}>{t.title}</a>,
   },
   {
     title: '状态',
     key: 'tags',
     dataIndex: 'tags',
-    render: tags => (
+    render: t => (
       <span>
-        {tags.map(tag => {
+        {t.tags.map(tag => {
           let color = tag === 'pass' ? 'green' : 'geekblue';
           if (tag === 'error') {
             color = 'volcano';
           }
           return (
-            <Tag color={color} key={tag}>
+            <Tag color={color} key={tag} onClick={()=>{alert(t.output)}}>
               {tag.toUpperCase()}
             </Tag>
           );
@@ -95,70 +108,14 @@ const commit_columns = [
   }
 ]
 
-const commit_data = [
-  {
-    key: '1',
-    commit_id: 1,
-    problem_title: {title: '两数之和', link: "/pid/1"},
-    tags: ['error']
-  },
-  {
-    key: '2',
-    commit_id: 2,
-    problem_title: {title: '两数之和', link: "/pid/1"},
-    tags: ['pass']
-  },
-  {
-    key: '2',
-    commit_id: 2,
-    problem_title: {title: '二叉树中序遍历', link: '/pid/3'},
-    tags: ['pending']
-  },
-]
-
 const problems_data = [
   {
     key: '1',
     problem_id: 1,
-    problem_title: {title: '两数之和', link: "/pid/1"},
-    problem_outline: '输入两个整数A和B，输出A+B的值',
-    tags: ['easy'],
-  },
-  {
-    key: '2',
-    problem_id: 2,
-    problem_title: {title: '反转链表', link: '/pid/2'},
-    problem_outline: '反转链表',
-    tags: ['medium'],
-  },
-  {
-    key: '3',
-    problem_id: 3,
-    problem_title: {title: '二叉树中序遍历', link: '/pid/3'},
-    problem_outline: '二叉树中序遍历',
+    problem_title: {title: '排序', link: "/writecode"},
+    problem_outline: '将数组从小到大排序',
     tags: ['hard'],
-  },
-  {
-    key: '4',
-    problem_id: 4,
-    problem_title: {title: '二叉树中序遍历', link: '/pid/3'},
-    problem_outline: '二叉树中序遍历',
-    tags: ['hard'],
-  },
-  {
-    key: '5',
-    problem_id: 5,
-    problem_title: {title: '二叉树中序遍历', link: '/pid/3'},
-    problem_outline: '二叉树中序遍历',
-    tags: ['hard'],
-  },
-  {
-    key: '6',
-    problem_id: 6,
-    problem_title: {title: '二叉树中序遍历', link: '/pid/3'},
-    problem_outline: '二叉树中序遍历',
-    tags: ['hard'],
-  },
+  }
 ];
 
 
@@ -168,15 +125,80 @@ function ProblemList(maxNumber) {
   );
 }
 
-function CommitList(maxNumber) {
-  return (
-    <Table columns={commit_columns} dataSource={commit_data} />
-  );
+class CommitList extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      commit_list: [
+      ]
+    };
+  }
+
+  componentDidMount() {
+    // alert("componentDidMount!!!!")
+
+    // try {
+    //   const response = await axios.get('localhost:8080/api/v1/commitlist');
+    //   console.log(response);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+    const _this = this
+    axios.get('/api/v1/commitlist')
+    .then(function (response) {
+      
+      let cl = response.data.commitlist
+      console.log(cl);
+      // handle success
+      let commitlist = []
+      for(let i=0;i<cl.length;i++){
+        console.log(cl[i])
+        let obj = {}
+        obj.key = i
+        obj.commit_id = cl[i]["ID"]
+        obj.commit_time = cl[i]["CreatedAt"]
+        obj.problem_title = {
+          title: '排序',
+          link: '/writecode'
+        }
+        if(cl[i]["Status"] == 0){
+          obj.tags = {tags: ['pending'], output: cl[i]["Output"]}
+        }
+        if(cl[i]["Status"] == 1){
+          obj.tags = {tags: ['pass'], output: cl[i]["Output"]}
+        }
+        if(cl[i]["Status"] == -1){
+          obj.tags = {tags: ['error'], output: cl[i]["Output"]}
+        }
+        commitlist.push(obj)
+      }
+      _this.setState({commit_list: commitlist})
+    })
+    .catch(function (error) {
+      // handle error
+      alert(error)
+      console.log(error);
+    })
+  }
+
+  render() {
+    return (
+      <Table columns={commit_columns} dataSource={this.state.commit_list} />
+    );
+  }
+  
 }
 
 const commit = (pid, code) => {
   return (event) => {
-    alert(code)
+    axios.post("/api/v1/commit",{
+      code: code
+    }).then(function (response) {
+      console.log(response)
+    }).catch(function (error) {
+      console.log(error)
+    })
   }
 }
 
@@ -195,8 +217,8 @@ class WriteCode extends React.Component {
       <div>
         <Row>
           <Col span={12}>
-            <h2>{this.state.global_pid}</h2>
-            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+            <h2>排序</h2>
+            <p>输入一个数n，然后输入n个整数，请你输出这个数组从小到大排序后的结果</p>
           </Col>
           <Col span={12}>
             <TextArea rows={25} onChange={
@@ -219,41 +241,57 @@ const NoMatch = () => (
   </div>
 )
 
-const AppRouter = () => (
-  <Router>
-    <Layout>
-      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key='1'>
-            <Link to="/commits/">提交记录</Link>
-          </Menu.Item>
-          <Menu.Item key='2'>
-            <Link to="/problems/">题目列表</Link>
-          </Menu.Item>
-          <Menu.Item key='3'>
-            <Link to="/writecode/">做题</Link>
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px', marginTop: 64 }}>
-        <div style={{ background: '#fff', padding: 24, minHeight: 500 }}>
-          <Switch>
-            <Route path="/commits/" exact component={CommitList} />
-            <Route path="/problems/" exact component={ProblemList} />
-            <Route path="/writecode/" exact component={WriteCode} />
-            <Redirect from="/*" to="/" />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Content>
-    </Layout>
-  </Router>
-);
+class AppRouter extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected_key: '2',
+    };
+  }
+
+  componentDidUpdate() {
+    // this.setState({selected_key: ''})
+  }
+
+  render(){
+    return (
+      <Router>
+        <Layout>
+          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={[window.menukey]}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key='1'>
+                <Link to="/commits/">提交记录</Link>
+              </Menu.Item>
+              <Menu.Item key='2'>
+                <Link to="/problems/">题目列表</Link>
+              </Menu.Item>
+              <Menu.Item key='3'>
+                <Link to="/writecode/">做题</Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 500 }}>
+              <Switch>
+                <Route path="/commits/" exact component={CommitList} />
+                <Route path="/problems/" exact component={ProblemList} />
+                <Route path="/writecode/" exact component={WriteCode} />
+                <Redirect from="/*" to="/" />
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
+          </Content>
+        </Layout>
+      </Router>
+    )
+  }
+}
 
 
 function App() {
